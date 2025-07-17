f346d3f8aed7fda47b55a524a59a06b4bd616c82
Usama Akhtar <contactwithusama142@gmail.com>
2025-07-17 17:20:43 +0500

    Admin Panel Setup

---
+++ resources/views/backend/inc/footer.blade.php
@@ -0,0 +1,7 @@
+ <footer class="main-footer">
+     <div class="footer-left">
+         <a href="#">Develop By Usama Akhtar @ {{ now('Y') }}</a></a>
+     </div>
+     <div class="footer-right">
+     </div>
+ </footer>
---
+++ resources/views/backend/layout/master.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- index.html  21 Nov 2019 03:44:50 GMT -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>@yield('page_title', 'Dashbard')</title>
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/app.min.css') }}">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/components.css') }}">
+    <!-- Custom style CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/custom.css') }}">
+    <link rel='shortcut icon' type='image/x-icon' href='assets/img/favicon.ico' />
+</head>
+
+<body>
+    <div class="loader"></div>
+    <div id="app">
+        <div class="main-wrapper main-wrapper-1">
+            <div class="navbar-bg"></div>
+            @include('backend.inc.nav')
+            @include('backend.inc.sidebar')
+            <!-- Main Content -->
+            <div class="main-content">
+                @yield('content')
+                @include('backend.inc.settings')
+            </div>
+            @include('backend.inc.footer')
+        </div>
+    </div>
+    <!-- General JS Scripts -->
+    <script src="{{ asset('backend/assets/js/app.min.js') }}"></script>
+    <!-- JS Libraies -->
+    <script src="{{ asset('backend/assets/bundles/apexcharts/apexcharts.min.js') }}"></script>
+    <!-- Page Specific JS File -->
+    <script src="{{ asset('backend/assets/js/page/index.js') }}"></script>
+    <!-- Template JS File -->
+    <script src="{{ asset('backend/assets/js/scripts.js') }}"></script>
+    <!-- Custom JS File -->
+    <script src="{{ asset('backend/assets/js/custom.js') }}"></script>
+</body>
+
+
+<!-- index.html  21 Nov 2019 03:47:04 GMT -->
+
+</html>
